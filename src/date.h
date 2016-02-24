d94bcff0186503ed800f52dc55a5a594c09cd1af
nsidaris <nsidaris@yahoo.com>
2016-02-23 20:18:47 -0800

    fixed date again
    fixed date again

--- src/date.h
+++ src/date.h
@@ -1,18 +1,103 @@
-#pragma once
+/*************************************************************************
+ * AUTHOR     : Nick Sidaris
+ * STUDENT ID : 385623
+ * ASSI #3    : Inheritance
+ * Class      : CS1C
+ * SECTION    : M/W 2:00pm
+ * DUE DATE   : 2/10/16
+ ************************************************************************/
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <sstream>
+#include<iomanip>
+using namespace std;
 
-class date {
-	int month;
-	int day;
-	int year;
+#ifndef DATE_H_
+#define DATE_H_
+/************************************************************************
+ * Date Class
+ * ______________________________________________________________________
+ * This class keeps track of 3 values that represent a date. The date can
+ * be set an validated. I made this back in CS1B and am reusing it. That
+ * is why the documentation is different(sorry!)
+ *
+ ***********************************************************************/
+class Date
+{
 public:
-	date();
-	date(int m, int d, int y);
-	void setMonth(int m);
-	int getMonth() const;
-	void setDay(int d);
-	int getDay() const;
-	void setYear(int y);
-	int getYear() const;
-	void printDate() const;
 
+	/***************************************************
+	 * CONSTRUCTORS & DESTRUCTOR
+	 **************************************************/
+	Date();
+
+	//uses private utility methods
+	Date(unsigned short month,
+			unsigned short day,
+			unsigned short year);
+
+	~Date();
+
+	/****************************************************
+	 * MUTATORS
+	 ***************************************************/
+	void SetDate(unsigned short month,
+			unsigned short day,
+			unsigned short year);
+
+	/****************************************************
+	 * ACCESSORS
+	 ***************************************************/
+	void GetDate(unsigned short &month,
+			unsigned short &day,
+			unsigned short &year) const;
+
+	unsigned short GetYear()  const;
+	unsigned short GetMonth() const;
+	unsigned short GetDay()   const;
+
+	string DisplayDate() const;
+
+private:
+
+	/****************************************************
+	 * UTILITIES METHODS
+	 ***************************************************/
+
+	unsigned short GetDaysInMonth(unsigned short month,
+			unsigned short year) const;
+
+	bool IsLeapYear(unsigned short year) const;
+
+	/****************************************************
+	 * VALIDATE METHODS
+	 ***************************************************/
+
+	//validates the month
+	bool ValidateMonth(unsigned short month) const;
+
+	// checks if day is valid takes year into consideration
+	bool ValidateDay(unsigned short month,
+			unsigned short day,
+			unsigned short year) const;
+
+	//Validates the year is between 1900 and current year
+	bool ValidateYear(unsigned short year) const;
+
+	// uses methods above to validate a given date
+	//checks against current day
+	bool ValidateDate(unsigned short month,
+			unsigned short day,
+			unsigned short year) const;
+
+	/****************************************************
+	 * ATTRIBUTES
+	 ***************************************************/
+	unsigned short dateMonth; //CALC - Month
+	unsigned short dateDay;   //CALC - day
+	unsigned short dateYear;  //CALC - year
 };
+
+
+#endif /* DATE_H_ */
